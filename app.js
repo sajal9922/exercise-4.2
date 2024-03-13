54874b76fc555082b0f3a0abdbc86b10a02f6ce3
Sajal <sajal.moni@gmail.com>
2024-03-13 13:59:42 +0200

    backend testing

--- app.js
+++ app.js
@@ -23,19 +23,6 @@ mongoose
     logger.error('error connecting to database', error.message);
   });
 
-// app.get('/api/blogs', (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs);
-//   });
-// });
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body);
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result);
-//   });
-// });
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
